f5efc00ba832a84d48bcc78d313c93ec5830c624
Artworqq <keysub.kevin@gmail.com>
2025-08-02 18:49:36 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  root: ".",
-  publicDir: "public",
+  root: ".", // ✅ ensure this points to where `index.html` is
+  build: {
+    rollupOptions: {
+      input: "public/index.html", // ⚠️ Only if you insist on keeping it in /public
+    },
+  },
 });
