5d8a82a601dfbfd7b9294bafc74556b6e7ee0ae7
Artworqq <keysub.kevin@gmail.com>
2025-08-02 18:17:51 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,10 @@
+// src/pages/Index.tsx
 import React from "react";
-import App from "../app";
 
 export default function Index() {
   return (
-    <App>
-      <h1 style={{ textAlign: "center", marginTop: "20vh", color: "#0ff" }}>
-        Hello World from SporeZ
-      </h1>
-    </App>
+    <div style={{ color: "white", fontSize: "2rem", textAlign: "center", marginTop: "4rem" }}>
+      Hello World from SporeZ 🌱
+    </div>
   );
 }
